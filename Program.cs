85ea32f89386261f0f3298caf9cf10ad39eac111
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-07-27 10:37:49 +0200

    example of using + opetator with strings, numbers. And simple example of +,-,/ and * operators.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello" + " world" + ". Osv...");
+﻿// Output a string in the console
+Console.WriteLine("A string");
+
+// Combine multiple strings with the + operator
+Console.WriteLine("Hello" + " world" + ". Osv...");
+
+// Print a string (that has a number)
+Console.WriteLine("2");
+// Print a number (an actual integer)
+Console.WriteLine(2);
+
+// String concatention:
+Console.WriteLine("2" + 2);
+// Also string concatention: 
+Console.WriteLine("2" + "2");
+
+// Math: addition
+Console.WriteLine(2 + 2);
+// Math: substraction
+Console.WriteLine(5 - 2);
+// Math: multiplication
+Console.WriteLine(5 * 3);
+// Math: division
+Console.WriteLine(10 / 2);
+
