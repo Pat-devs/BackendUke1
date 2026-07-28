01544ad357b2c24fea78cc87ea43b8d934bc51cb
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-07-28 10:49:08 +0200

    super simple calc with both sum,sub,mul and div

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// super simplified calculator
-// features: can receive two numbers
-// 
+// features: can receive two numbers, and show:
+// sum, substraction, multiplication and division.
 
 Console.Clear();
 Console.WriteLine("Welcome to super simple calculator!");
@@ -14,6 +14,19 @@ int secondNumber = int.Parse(Console.ReadLine());
 // Show the numbers as a sum equation, and the result
 int sumResult = firstNumber + secondNumber;
 
-Console.WriteLine(firstNumber + " + " + secondNumber + " = " + firstNumber + secondNumber);
-
 Console.WriteLine($"{firstNumber} + {secondNumber} = {sumResult}");       
+
+// Show the numbers as a substraction equation, and the result
+int subResult = firstNumber - secondNumber;
+
+Console.WriteLine($"{firstNumber} - {secondNumber} = {subResult}");   
+
+// Show the numbers as a multiplication equation, and the result
+int mulResult = firstNumber * secondNumber;
+
+Console.WriteLine($"{firstNumber} * {secondNumber} = {mulResult}");   
+
+// Show the numbers as a division equation, and the result
+double divResult = (double)firstNumber / secondNumber;
+
+Console.WriteLine($"{firstNumber} / {secondNumber} = {divResult}");  
