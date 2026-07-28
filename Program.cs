fe82100185b1a156b5a83782decb6c3eea4c6018
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-07-28 09:11:20 +0200

    int.Parse example usage

--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
 ﻿// basic greeter app :)
-string welcomeMessage = "Welcome, please enter your name.";
+//string welcomeMessage = "Welcome, please enter your name.";
 
 // Write welcome message to user
-Console.WriteLine(welcomeMessage);
+//Console.WriteLine(welcomeMessage);
 
 // Read input from console, and store it in a variable
-string userName = Console.ReadLine();
+// string userName = Console.ReadLine();
 
 // Greet the user by their name
-Console.WriteLine("Nice to meet you " + userName);
+//Console.WriteLine("Nice to meet you " + userName);
 
 // Ask user their age
-Console.WriteLine("Please enter your age");
+// Console.WriteLine("Please enter your age");
 
-var userAge = Console.ReadLine();
+//int userAge = Console.ReadLine();
 
 // Write to user how old they said they are...
 
-Console.WriteLine("You wrote that you are " + userAge + " years old.");
+//Console.WriteLine("You wrote that you are " + userAge + " years old.");
+
+int luckyNumber = int.Parse("2");
+
+Console.WriteLine(luckyNumber * 50);
