a191234aa9dee7a87ad1663d94ee1722429a106e
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-07-28 09:18:13 +0200

    using int.parse with Console.ReadLine

--- Program.cs
+++ Program.cs
@@ -1,24 +1,25 @@
 ﻿// basic greeter app :)
-//string welcomeMessage = "Welcome, please enter your name.";
+string welcomeMessage = "Welcome, please enter your name.";
 
 // Write welcome message to user
-//Console.WriteLine(welcomeMessage);
+Console.WriteLine(welcomeMessage);
 
 // Read input from console, and store it in a variable
-// string userName = Console.ReadLine();
+string userName = Console.ReadLine();
 
 // Greet the user by their name
-//Console.WriteLine("Nice to meet you " + userName);
+Console.WriteLine("Nice to meet you " + userName);
 
 // Ask user their age
-// Console.WriteLine("Please enter your age");
+Console.WriteLine("Please enter your age");
 
-//int userAge = Console.ReadLine();
+var userInputAge = Console.ReadLine(); // read the input, and store as a variable
+int userAge = int.Parse(userInputAge); // convert the input into an int
 
 // Write to user how old they said they are...
 
-//Console.WriteLine("You wrote that you are " + userAge + " years old.");
+Console.WriteLine("You wrote that you are " + userAge + " years old.");
 
-int luckyNumber = int.Parse("2");
+//int luckyNumber = int.Parse("2");
 
-Console.WriteLine(luckyNumber * 50);
+//Console.WriteLine(luckyNumber * 50);
