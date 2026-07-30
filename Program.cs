fb94f77a46482dfa5fded28dfecd3779535573f9
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-07-30 09:20:04 +0200

    if else statement example

--- Program.cs
+++ Program.cs
@@ -1,30 +1,14 @@
 ﻿Console.Clear();
 
+// most basic condition
+
+if (2 != 2)
 {
-    string user = "Patryk";
+    // code only runs if above condition is true
+    Console.WriteLine("Yes the condition was truthy, so i will show you this text.");
 }
-
+else 
 {
-    string user = "Bob";
-    //Console.WriteLine(user);
+    // code only runs if above condition is false
+    Console.WriteLine("Nope, the condition was false, so i will show you this text from the else block :)");
 }
-
-// == is the comparison operator
-Console.WriteLine(2 == 2); // true
-Console.WriteLine(2 == 3); // false
-
-Console.WriteLine(2 > 3); // false because 2 is not larger than 3
-Console.WriteLine(2 > 1); // true because 2 is larger than 1
-
-Console.WriteLine(5 >= 4); // larger or equal to something
-Console.WriteLine(5 >= 5); // larger or equal to something
-Console.WriteLine(4 >= 5); // larger or equal to something
-
-Console.WriteLine(5 <= 4); // less or equal to something
-Console.WriteLine(5 <= 5); // less or equal to something
-Console.WriteLine(4 <= 5); // less or equal to something
-
-// not equal
-Console.WriteLine(2 != 2); // is two not equal to two ? False
-Console.WriteLine(2 != 3); // is two not equal to two ? True
-
